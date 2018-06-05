55b05a6664d35b1a665380c19da6c6a4ef5e2c8e
Phill Hawthorne <fitz@Phills-MacBook-Pro.local>
2018-06-05 11:34:14 -0600

    Second Commit arraymethods and no duplicates

---
+++ exercises/arrrayMethodExercise/index.js
@@ -0,0 +1,8 @@
+var arr = [1,2,3, "phil"]
+
+
+
+
+console.log(arr.slice(2));
+
+console.log(arr)
---
+++ exercises/noDuplicates/index.js
@@ -0,0 +1,35 @@
+// function noDupes(str){
+//     var output = {
+//         noDupes: "",
+//         extras: ""
+//     }
+//     for  (i = 0; i < noDupes.length; i++){
+
+
+//     }
+// }
+
+// Given a string, extract duplicates and leave the rest
+// Loop through the string 
+// Determine if the character has already been seen
+// if true add to extra
+// otherwise add to noDupes
+// return output
+// var input = "bookkeper larry";
+// let duplicateRemover = function(str) {
+//     var output = {
+//         noDupes: "",
+//         extras: ""
+//     }
+//     for (i = 0; i < str.length; i++) {
+//         if(output.noDupes.includes(str[i])){
+//             output.extras += str[i];
+
+//         }else {
+//             output.noDupes += str[i];
+//         }
+//     }
+//     return output;
+// }
+// console.log(duplicateRemover("bookkeeper larry"));
+
